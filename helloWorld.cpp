f53671ea57c966150b53002bad16236e46b3b268
Asya <111695385+asyanix@users.noreply.github.com>
2024-11-14 14:42:23 +0300

    Update helloWorld.cpp file, add user greeting

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -2,6 +2,9 @@
 using namespace std; 
 
 int main() {
-   cout << "Hello, World!" << endl;
+   string name;
+   cout << "Enter your name: ";
+   cin >> name; 
+   cout << "Hello, World from " << name << "!" << endl; 
    return 0; 
 }
